8dba72b722fe57044e1e3f359437f3550eae9d4a
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-06-01 09:26:54 +0530

    Basic Setup is there..working on second componenet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,5 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-		},
-	},
 	plugins: [],
 }
