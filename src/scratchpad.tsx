234680c1bcffbb4243da5db627f94a04f6bc2c70
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-06-01 21:30:24 +0530

    Completed Hero Section

---
+++ src/scratchpad.tsx
@@ -0,0 +1,11 @@
+
+const styles = {
+    container: {
+        width: '100%',
+        display: 'flex',
+        // do a flip animation
+        transform: 'rotateY(180deg)',
+        transition: 'transform 0.5s'
+    },
+    
+}
